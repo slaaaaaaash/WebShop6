943fb97615f28edf669ef959c9a2ddda8b61ae5c
SvenskaBarnet <63249655+SvenskaBarnet@users.noreply.github.com>
2023-10-09 21:08:29 +0200

    A working function for logging in
    It logs checks if username exists and if the password is correct. for
    now it's case sensitive but it doesn't have to be. There's also a
    function commented out for checking if user is an Admin or a Customer.
    We can decide later if that is supposed to happen here in the log in or
    somewhere else in the program.

--- Login.cs
+++ Login.cs
@@ -3,13 +3,184 @@
 namespace WebShop6;
 public class Login
 {
-    /*
-        public static string LogInUser()
+    public static string AddUsername()
+    {
+        string UserName;
+        bool validUser;
+        do
         {
-
+            bool validInput;
+            do
+            {
+                validInput = true;
+                Console.WriteLine("Please enter username and password to log in:");
+                Console.Write("username: ");
+                UserName = Console.ReadLine();
+                if (UserName.Length <= 0)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Please make sure you've actually entered a username.");
+                    Console.WriteLine("Press any key to try again");
+                    Console.ReadKey();
+                    Console.Clear();
+                    validInput = false;
+                }
+            } while (!validInput);
+            if (!ValidateUsername(UserName))
+            {
+                Console.WriteLine();
+                Console.WriteLine("Username does not exist, press any key to try again");
+                Console.ReadKey();
+                Console.Clear();
+                AddUsername();
+            }
+            else
+                validUser = true;
+            return UserName;
+        } while (!validUser);
+    }
+   
+    public static string AddPassword(string username)
+    {
+        string UserName = username;
+        string Password;
+        bool validPass;
+        do
+        {
+            bool validInput;
+            do
+            {
+                validInput = true;
+                Console.Clear();
+                Console.WriteLine("Please enter username and password to log in:");
+                Console.Write("username: ");
+                Console.WriteLine(UserName);
+                Console.Write("Password: ");
+                Password = Login.MaskedPass();
+                Console.WriteLine();
+                if (Password.Length <= 0)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Please make sure you've actually entered a password.");
+                    Console.WriteLine("Press any key to try again");
+                    Console.ReadKey();
+                    Console.Clear();
+                    validInput = false;
+                }
+            } while (!validInput);
+            if (!ValidatePassword(UserName, Password))
+            {
+                Console.WriteLine();
+                Console.WriteLine("You've entered the incorrect password, press any key to try again.");
+                Console.ReadKey();
+                Console.Clear();
+                AddPassword(UserName);
+            }
+            else
+                validPass = true;
+            return Password;
+        } while (!validPass);
+    }
+    public static bool ValidateUsername(string username)
+    {
+        string user = username;
+        string[] customers = File.ReadAllLines("../../../Customers.csv");
+        string[] admins = File.ReadAllLines("../../../Admins.csv");
+        bool validUsername = false;
+        foreach (string customer in customers)
+        {
+            string[] splitCustomer = customer.Split(",");
+            if (splitCustomer[0] == user)
+            {
+                validUsername = true;
+            }
         }
-    */
 
+        foreach (string admin in admins)
+        {
+            string[] splitAdmin = admin.Split(",");
+            if (splitAdmin[0] == user)
+            {
+                validUsername = true;
+            }
+        }
+
+        if (validUsername)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static bool ValidatePassword(string username, string password)
+    {
+        string user = username;
+        string pass = password;
+        string[] customers = File.ReadAllLines("../../../Customers.csv");
+        string[] admins = File.ReadAllLines("../../../Admins.csv");
+        bool validPass = false;
+        foreach (string customer in customers)
+        {
+            string[] splitCustomer = customer.Split(",");
+            if (splitCustomer[0] == user && splitCustomer[1] == pass)
+            {
+                validPass = true;
+            }
+        }
+
+        foreach (string admin in admins)
+        {
+            string[] splitAdmin = admin.Split(",");
+            if (splitAdmin[0] == user && splitAdmin[1] == pass)
+            {
+                validPass = true;
+            }
+        }
+
+        if (validPass == true)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+
+    public static string MaskedPass()
+    {
+        string pass = string.Empty;
+        ConsoleKeyInfo key;
+        do
+        {
+            key = Console.ReadKey(true);
+            if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+            {
+                pass += key.KeyChar;
+                Console.Write("*");
+            }
+            else
+            {
+                if (key.Key == ConsoleKey.Backspace && pass.Length > 0)
+                {
+                    pass = pass.Substring(0, (pass.Length - 1));
+                    Console.Write("\b \b");
+                }
+                else if (key.Key == ConsoleKey.Enter)
+                {
+                    break;
+                }
+            }
+        } while (key.Key != ConsoleKey.Enter);
+        return pass;
+    }
+    
+    /* working function for finding out if user is an Admin or a Customer we might want this to happen here in at the login or somewhere else in the program.
+     
     public static Enum ValidateUsername(string username)
     {
         string user = username;
@@ -48,45 +219,5 @@ public class Login
         }
     }
 
-    
-    /*
-    public static bool ValidatePassword(string username, string password)
-    {
-        switch (ImportUserLogin("../../../" + ValidateUsername(username) + ".csv")[username] == password)
-        {
-            case true:
-                return true;
-            case false:
-                return false;
-        }
-    }
-
     */
-    public static string MaskedPass()
-    {
-        string pass = string.Empty;
-        ConsoleKeyInfo key;
-        do
-        {
-            key = Console.ReadKey(true);
-            if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
-            {
-                pass += key.KeyChar;
-                Console.Write("*");
-            }
-            else
-            {
-                if (key.Key == ConsoleKey.Backspace && pass.Length > 0)
-                {
-                    pass = pass.Substring(0, (pass.Length - 1));
-                    Console.Write("\b \b");
-                }
-                else if (key.Key == ConsoleKey.Enter)
-                {
-                    break;
-                }
-            }
-        } while (key.Key != ConsoleKey.Enter);
-        return pass;
-    }
 }
--- Program.cs
+++ Program.cs
@@ -1,43 +1,7 @@
 ﻿using WebShop6;
 
-string? UserName;
-string? UserPassword;
-bool validInput = false;
+string? UserName = Login.AddUsername();
+string? UserPassword = Login.AddPassword(UserName);
 
-do
-{
-    validInput = true;
-    Console.Clear();
-    Console.WriteLine("Welcome to the time-travelling Feline!");
-    Console.WriteLine();
-    Console.WriteLine("Please enter username and password to log in:");
-    Console.Write("username: ");
-    UserName = Console.ReadLine();
-    if (UserName.Length <= 0)
-    {
-        Console.WriteLine();
-        Console.WriteLine("Please make sure you've actually entered a username.");
-        Console.WriteLine("Press any key to try again");
-        Console.ReadKey();
-        Console.Clear();
-        validInput = false;
-    }
-    else
-    {
-        Console.Write("Password: ");
-        UserPassword = Login.MaskedPass();
-        Console.WriteLine();
-        if (UserPassword.Length <= 0 || UserName.Length <= 0)
-        {
-            Console.WriteLine();
-            Console.WriteLine("Please make sure you've actually entered a password.");
-            Console.WriteLine("Press any key to try again");
-            Console.ReadKey();
-            Console.Clear();
-            validInput = false;
-        }
-    }
-} while (!validInput);
 
-Console.WriteLine(Login.ValidateUsername(UserName));
 
