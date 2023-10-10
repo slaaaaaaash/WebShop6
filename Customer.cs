f2ab29edc9fc0f2c4bd171175d1550c11177ce9d
SvenskaBarnet <63249655+SvenskaBarnet@users.noreply.github.com>
2023-10-10 12:13:49 +0200

    Added layout for customer menu

--- Customer.cs
+++ Customer.cs
@@ -13,12 +13,50 @@
         public static void CustomerMenu(string username)
         {
             Console.Clear();
-            Console.WriteLine(username + "! Welcome to the Customer Menu!");
-            Console.WriteLine();
-            Console.WriteLine("Choice 1");
-            Console.WriteLine("Choice 2");
-            Console.WriteLine("Choice 3");
-            Console.WriteLine("Choice 4");
+            Console.WriteLine($"******************************************************************");
+            Console.WriteLine($"****************************************************************** \n");
+            Console.WriteLine(username + "! WELCOME to: The Admin menu\n");
+            Console.WriteLine(" 1. Product List");
+            Console.WriteLine(" 2. Order History\n");
+            Console.WriteLine(" 3. Shopping Basket\n");
+            Console.WriteLine(" 0. Log out\n");
+            Console.WriteLine($"******************************************************************");
+            Console.WriteLine($"****************************************************************** \n");
+
+            bool isSucceed = int.TryParse(Console.ReadLine(), out int choice);
+
+            if (isSucceed)
+            {
+                switch (choice)
+                {
+                    case 0:
+                        Start.ShowStart();
+                        break;
+
+                    case 1: //Product List
+                        break;
+
+                    case 2: //Order History
+                        break;
+
+                    case 3: //Shopping Basket
+                        break;
+
+                    default: //ogiltig siffra matas in
+                        Console.WriteLine(" Invalid choice. Try again!");
+                        Thread.Sleep(1000);
+                        CustomerMenu(username);
+                        break;
+                }
+            }
+            else //ogiltig symbol matas in
+            {
+                Console.WriteLine("Invalid input. Try again!");
+                Thread.Sleep(1000);
+                CustomerMenu(username);
+            }
+
         }
+
     }
 }
