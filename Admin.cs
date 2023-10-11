66963f82927e08790efc1181891df9c64bd523a3
fabian withycombe <fabian.withycombe@hotmail.com>
2023-10-11 16:18:24 +0200

    Admin add remove and edit fucntions

--- Admin.cs
+++ Admin.cs
@@ -43,11 +43,12 @@
                         Console.Clear();
                         Console.WriteLine("1. Add Product");
                         Console.WriteLine("2. Remove Product");
-                        Console.WriteLine("3. Back");
+                        Console.WriteLine("3. Edit Product");
+                        Console.WriteLine("4. Back");
 
                         bool addRemChoice = int.TryParse(Console.ReadLine(), out int addOrRemove);
 
-                        string newItemName, newItemPrice;
+                        string newItemName, newItemPrice, editItem;
 
                         if (addRemChoice)
                         {
@@ -55,41 +56,81 @@
                             {
                                 case 1: //add product
                                     Console.Clear();
+
                                     Console.WriteLine("Add a new product");
                                     Console.WriteLine("Name of product:");
-
                                     newItemName = Console.ReadLine();
 
                                     Console.Clear();
-                                    Console.WriteLine("Price of product:");
 
+                                    Console.WriteLine("Price of product:");
                                     newItemPrice = Console.ReadLine();
 
+                                    string[] products = File.ReadAllLines("../../../Products.csv");
+
+                                    string[] nameAndPrice = { newItemName + "," + newItemPrice };
+                                    File.AppendAllLines("../../../Products.csv", nameAndPrice);
+
                                     Console.Clear();
-
-                                    // string path = @"../../../Product.csv";
-
-
 
                                     break;
 
                                 case 2: //remove product
+                                    string[] productsList = File.ReadAllLines("../../../Products.csv");
+
+                                    Console.Clear();
+
+                                    foreach (string product in productsList)
+                                    {
+                                        Console.WriteLine(product);
+                                    }
+
+                                    Console.WriteLine("Choose a product to remove");
+
+                                    bool remChoice = int.TryParse(Console.ReadLine(), out int Remove);
+                                    Remove = Remove - 1;
+
+                                    List<string> list = File.ReadAllLines("../../../Products.csv").ToList();
+                                    string itemRem = list[Remove];
+                                    list.RemoveAt(Remove);
+                                    File.WriteAllLines("../../../Products.csv", list.ToArray());
 
                                     break;
 
-                                case 3: //back
+                                case 3: //Edit product WIP
+
+                                    string[] productsListEdit = File.ReadAllLines("../../../Products.csv");
+
+                                    Console.Clear();
+
+                                    foreach (string product in productsListEdit)
+                                    {
+                                        Console.WriteLine(product);
+                                    }
+
+                                    Console.WriteLine("Choose a product to edit");
+                                    bool editChoice = int.TryParse(Console.ReadLine(), out int Edit);
+                                    Edit = Edit - 1;
+
+                                    List<string> editList = File.ReadAllLines("../../../Products.csv").ToList();
+                                    string itemEdit = editList[Edit];
+
+                                    Console.WriteLine("The items new name and price:");
+                                    editItem = Console.ReadLine();
+
+                                    editList[Edit] = editItem;
+                                    File.WriteAllLines("../../../Products.csv", editList.ToArray());
+
+                                    break;
+
+
+                                case 4: //back
 
                                     break;
                             }
                         }
 
-
-
-
-
-
                         break;
-
 
                     case 3: //Manage Orders
                         break;
