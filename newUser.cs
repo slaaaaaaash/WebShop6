fc529bb260be1b64b5fc35a5c84983102a4175eb
johanilsson <jnilsson.malmo@gmail.com>
2023-10-12 10:33:52 +0200

    Used Klas functions for nice look

--- newUser.cs
+++ newUser.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
 namespace WebShop6;
 
 public class newUser
@@ -15,55 +17,59 @@ public class newUser
         string newPass, newUser, tempPass;
         if (freshUser = true)
         {
-            Console.Write("Please enter Username: ");
-            newUser = Console.ReadLine();
-            if (newUser.Length <= 0)
-                do
-                {
-                    Console.Write("Try again: ");
-                    newUser = Console.ReadLine();
-                }
-                while (newUser.Length <= 0);
-            string[] usersLines = File.ReadAllLines("../../../newUser.csv");
-            for (int i = 0; i < usersLines.Length; i++)
-            {
-                string usertemp = usersLines[i];
-                string[] user = usertemp.Split(",");
-                do
-                {
-                    if (newUser == user[0])
-                    {
-                        Console.WriteLine(user[0] + " already exists");
-                        Console.Write("Chose a diffrent username: ");
-                        newUser = Console.ReadLine();
-                    }
-                }
-                while (newUser == user[0]);
-            }
-            int count = 1;
-            do
-            {
-                Console.Write("Please enter a secure password: ");
-                newPass = Login.MaskedPass();
-                Console.WriteLine("");
-                Console.Write("Please enter your password one more time: ");
-                tempPass = Login.MaskedPass();
-                if (newPass != tempPass)
-                {
+            Console.WriteLine("To return to previous meny, press ESC.");
+            Console.WriteLine();
+            Console.WriteLine("Otherwise, press any key to log in");
+            if (Console.ReadKey(true).Key != ConsoleKey.Escape)
+                Console.Write("Please enter Username: ");
+                newUser = Console.ReadLine();
+
+
+                if (newUser.Length <= 0)
                     do
                     {
-                        Console.WriteLine("");
-                        Console.Write("WRONG! Please reenter your password: " + count + "  / 3 attempt: ");
-                        tempPass = Login.MaskedPass();
-                        count++;
+                        Console.Write("Try again: ");
+                        newUser = Console.ReadLine();
                     }
-                    while (count <=3);
+                    while (newUser.Length <= 0);
+                do
+                    if (Login.ValidateUsername(newUser))
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Welcome to the Time-travelling Feline");
+                        Console.Write(newUser + " exist try again: ");
+                        newUser = Console.ReadLine();
+                    }
+                while (Login.ValidateUsername(newUser));
+                int count = 1;
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter Username: " + newUser);
+                    Console.Write("Please enter a secure password: ");
+                    newPass = Login.MaskedPass();
                     Console.WriteLine("");
+                    Console.Write("Please enter your password one more time: ");
+                    tempPass = Login.MaskedPass();
+                    if (newPass != tempPass)
+                    {
+                        do
+                        {
+                            Console.WriteLine("");
+                            Console.Write("WRONG! Please reenter your password: " + count + "  / 3 attempt: ");
+                            tempPass = Login.MaskedPass();
+                            count++;
+                        }
+                        while (count <= 3);
+                        Console.WriteLine("");
+                    }
+                    string[] userAndPass = { newUser + "," + newPass };
+                    File.AppendAllLines("../../../newUser.csv", userAndPass);
                 }
-                string[] userAndPass = { newUser + "," + newPass };
-                File.AppendAllLines("../../../newUser.csv", userAndPass);
-            }
             while (newPass != tempPass);
         }
     }
 }
+
+    
+
