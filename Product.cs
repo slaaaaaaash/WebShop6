93550a6ae35e5ba393d3701414c5564929d70b93
SvenskaBarnet <63249655+SvenskaBarnet@users.noreply.github.com>
2023-10-15 18:09:10 +0200

    Fixed menus, product creation and product browsing.
    You can now browse the menus as customer, see the full product list and
    there is a constructor for creating products.

--- Product.cs
+++ Product.cs
@@ -1,8 +1,27 @@
-﻿namespace WebShop6
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace WebShop6;
+
+public class Product
 {
-    public class Product
+    public readonly string? Name;
+    public readonly int Price;
+    public readonly string? Description;
+
+
+    public Product(string[] info)
     {
-        public readonly string? Name;
-        public readonly int Price;
+        Name = info[0];
+        if (int.TryParse(info[1], out int value))
+        {
+            Price = value;
+        }
+        else
+        {
+            throw new Exception();
+        }
+        Description = info[2];
     }
+
 }
--- Program.cs
+++ Program.cs
@@ -1,69 +1,141 @@
 ﻿using WebShop6;
 
 IUser? user = null;
-
-do 
+bool logOut;
+do
 {
-    Console.Clear();
-    Console.WriteLine($"******************************************************************");
-    Console.WriteLine($"****************************************************************** \n");
-    Console.WriteLine(" WELCOME to: The Time-travelling Feline Shop\n");
-    Console.WriteLine(" 1. Login ");
-    Console.WriteLine(" 2. Register\n");
-    Console.WriteLine(" 0. Exit\n");
-    Console.WriteLine($"******************************************************************");
-    Console.WriteLine($"****************************************************************** \n");
-
-    bool isSucceed = int.TryParse(Console.ReadLine(), out int choice);
-
-    if (isSucceed)
+    logOut = false;
+    do
     {
-        switch (choice)
+        Console.Clear();
+        Console.WriteLine($"******************************************************************");
+        Console.WriteLine($"****************************************************************** \n");
+        Console.WriteLine(" WELCOME to: The Time-travelling Feline Shop\n");
+        Console.WriteLine(" 1. Login ");
+        Console.WriteLine(" 2. Register\n");
+        Console.WriteLine(" 0. Exit\n");
+        Console.WriteLine($"******************************************************************");
+        Console.WriteLine($"****************************************************************** \n");
+
+        bool isSucceed = int.TryParse(Console.ReadLine(), out int choice);
+
+        if (isSucceed)
         {
-            case 0:
-                Console.Clear();
-                Console.WriteLine("*******************************************************************");
-                Console.WriteLine("*******************************************************************\n");
-                Console.WriteLine(" Welcome back!\n");
-                Console.WriteLine("*******************************************************************");
-                Console.WriteLine("*******************************************************************\n");
-                Thread.Sleep(3000);
-                Console.Clear();
-                Console.WriteLine("Program has exited.");
-                Environment.Exit(1);
-                break;
+            switch (choice)
+            {
+                case 0:
+                    Console.Clear();
+                    Console.WriteLine("*******************************************************************");
+                    Console.WriteLine("*******************************************************************\n");
+                    Console.WriteLine(" Welcome back!\n");
+                    Console.WriteLine("*******************************************************************");
+                    Console.WriteLine("*******************************************************************\n");
+                    Thread.Sleep(3000);
+                    Console.Clear();
+                    Console.WriteLine("Program has exited.");
+                    Environment.Exit(1);
+                    break;
 
-            case 1: //login 
-                user = LoginMenu.Login();
-                break;
+                case 1: //login 
+                    user = LoginMenu.Login();
+                    break;
 
-            case 2: // register new customer 
-                LoginMenu.Register();
-                break;
+                case 2: // register new customer 
+                    LoginMenu.Register();
+                    break;
 
-            default: //invalid input
-                Console.WriteLine(" Invalid choice. Try again!");
-                Thread.Sleep(1000);
-                break;
+                default: //invalid input
+                    Console.WriteLine(" Invalid choice. Try again!");
+                    Thread.Sleep(1000);
+                    break;
+            }
         }
-    }
-    else //invalid input
-    {
-        Console.WriteLine("Invalid input. Try again!");
-        Thread.Sleep(1000);
-    }
-} while (user == null);
+        else //invalid input
+        {
+            Console.WriteLine("Invalid input. Try again!");
+            Thread.Sleep(1000);
+        }
+    } while (user == null);
 
-if (user is Customer c)
-{
-    Console.Clear();
-    Console.WriteLine("\nCongrats you've succesfully logged in as a customer");
-    Console.WriteLine(c.Username);
-    Console.WriteLine(c.Cart);
-}
-else if (user is Admin a)
-{
-    Console.Clear();
-    Console.WriteLine("\nCongrats you've succesfully logged in as an admin");
-    Console.WriteLine(a.Username);
-}
+    if (user is Customer c)
+    {
+        bool customerReturn;
+        do
+        {
+            customerReturn = true;
+            Console.Clear();
+            Console.WriteLine($"******************************************************************");
+            Console.WriteLine($"****************************************************************** \n");
+            Console.WriteLine(c.Username.ToUpper() + "! WELCOME to: The Time-travelling Feline Shop\n");
+            Console.WriteLine(" 1. Product List ");
+            Console.WriteLine(" 2. Order History\n");
+            Console.WriteLine(" 3. View Shopping Cart\n");
+            Console.WriteLine(" 0. Log out\n");
+            Console.WriteLine($"******************************************************************");
+            Console.WriteLine($"****************************************************************** \n");
+
+            bool isSucceed = int.TryParse(Console.ReadLine(), out int choice);
+
+            if (isSucceed)
+            {
+                switch (choice)
+                {
+                    case 0:
+                        logOut = true;
+                        break;
+
+                    case 1: //Product List 
+                        Console.Clear();
+                        string[] products = File.ReadAllLines("products.csv");
+                        List<Product> listProducts = new List<Product>();
+                        foreach (string line in products)
+                        {
+                            string[] info = line.Split(',');
+                            Product product = new Product(info);
+                            listProducts.Add(product);
+                        }
+                        for (int i = 0; i < listProducts.Count; i++)
+                        {
+                            Product line = listProducts[i];
+                            Console.WriteLine(line.Name + " [" + (i + 1) + "]");
+                            Console.WriteLine("Price: " + line.Price + "kr");
+                            Console.WriteLine("Description: " + line.Description + "\n");
+                        }
+                        Console.ReadKey();
+                        customerReturn = false;
+                        break;
+
+                    case 2: // Order History 
+                        customerReturn = false;
+                        break;
+
+                    case 3: // Shopping Cart
+                        customerReturn = false;
+                        break;
+
+                    default: //invalid input
+                        Console.WriteLine(" Invalid choice. Try again!");
+                        customerReturn = false;
+                        Thread.Sleep(1000);
+                        break;
+                }
+            }
+            else //invalid input
+            {
+                Console.WriteLine("Invalid input. Try again!");
+                customerReturn = false;
+                Thread.Sleep(1000);
+            }
+        } while (!customerReturn);
+
+
+
+    }
+    else if (user is Admin a)
+    {
+        Console.Clear();
+        Console.WriteLine("\nCongrats you've succesfully logged in as an admin");
+        Console.WriteLine(a.Username);
+    }
+
+} while (logOut);
