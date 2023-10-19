1cc5468d54be7ec75cbac81dfa26141c9a71b617
= <klas_hermansson@hotmail.com>
2023-10-19 13:22:30 +0200

    Moved customer menu to separate class
    It makes more sense than a customer containing a menu.

---
+++ CustomerMenu.cs
@@ -0,0 +1,63 @@
+﻿namespace WebShop6;
+
+public class CustomerMenu
+{
+        public static void Main(string username)
+        {
+
+            Console.Clear();
+            Console.WriteLine($"******************************************************************");
+            Console.WriteLine($"****************************************************************** \n");
+            Console.WriteLine(username + "! WELCOME to: The Customer menu\n");
+            Console.WriteLine(" 1. Product List");
+            Console.WriteLine(" 2. Order History\n");
+            Console.WriteLine(" 3. Shopping Basket\n");
+            Console.WriteLine(" 0. Log out\n");
+            Console.WriteLine($"******************************************************************");
+            Console.WriteLine($"****************************************************************** \n");
+
+            bool isSucceed = int.TryParse(Console.ReadLine(), out int choice);
+            string[] productsList = File.ReadAllLines("../../../Products.csv");
+
+            if (isSucceed)
+            {
+                switch (choice)
+                {
+                    case 0:
+
+                        Start.ShowStart();
+                        break;
+
+                    case 1: //Product List
+                        Console.Clear();
+                        Console.WriteLine("Products:");
+                        Console.WriteLine("Name  " + "Price");
+                        Console.WriteLine();
+
+                        foreach (string product in productsList)
+                        {
+                            Console.WriteLine(product);
+                        }
+                        break;
+
+                    case 2: //Order History
+                        break;
+
+                    case 3: //Shopping Basket
+                        break;
+
+                    default: //ogiltig siffra matas in
+                        Console.WriteLine(" Invalid choice. Try again!");
+                        Thread.Sleep(1000);
+                        Main(username);
+                        break;
+                }
+            }
+            else //ogiltig symbol matas in
+            {
+                Console.WriteLine("Invalid input. Try again!");
+                Thread.Sleep(1000);
+                Main(username);
+            }
+        }
+}
